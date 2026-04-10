5c4c154ae6002af408321486acb8529f9b91f23c
Matheus Silva <mathsilva.code@gmail.com>
2026-04-10 15:51:27 -0300

    feat: implements ContactRepository and CustomerRepository

---
+++ src/main/java/com/matheus/minicrm/contact/repository/ContactRepository.java
@@ -0,0 +1,9 @@
+package com.matheus.minicrm.contact.repository;
+
+import com.matheus.minicrm.contact.entity.Contact;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ContactRepository extends JpaRepository<Contact, Long> {
+}

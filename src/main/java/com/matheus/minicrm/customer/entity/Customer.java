5084910d585c8af64f13c025f0d4986ba287879e
Matheus Silva <mathsilva.code@gmail.com>
2026-04-10 14:43:26 -0300

    feat: implements Customer Entity

---
+++ src/main/java/com/matheus/minicrm/customer/entity/Customer.java
@@ -0,0 +1,23 @@
+package com.matheus.minicrm.customer.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "customers")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+}
